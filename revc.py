1d92ddf63b3248076eaf5c746479f59d34c8489f
bakuncwa <98463078+bakuncwa@users.noreply.github.com>
2024-01-02 13:07:49 +0400

    Update revc.py

--- revc.py
+++ revc.py
@@ -1,3 +1,7 @@
+# REVC: Complementing a Stand of DNA
+# Given:  A DNA string s of length at most 1000 bp.
+# Return: The reverse complement sc of s.
+
 def reverse_comp(seq):
     comp = {"A": "T", "T": "A", "C": "G", "G": "C"}
     DNA_strand = " "
