3d3fca89dd2790f5806448d53ab59251ba2e3e84
bakuncwa <98463078+bakuncwa@users.noreply.github.com>
2024-01-02 12:45:51 +0400

    Add files via upload

---
+++ ini4.py
@@ -0,0 +1,19 @@
+# INI4: Conditions and Loops
+# Given: Two positive integers a and b (a<b<10000)
+# Return: The sum of all odd integers from a through b, inclusively.
+
+def odd_total(a, b):
+    return sum([i for i in range(a, b) if i % 2 == 1])
+
+ros_f = open("/Users/ . . /Downloads/rosalind_ini4.txt")
+a, b = map(int, ros_f.readline().rstrip().rsplit())
+print(odd_total(a, b))
+
+# print(odd_total(100, 200))
+# Ans: 7500
+
+
+
+            
+            
+             
