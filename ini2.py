8df583cf3735b47bd1e6cfe19b308d0ea24d43a3
bakuncwa <98463078+bakuncwa@users.noreply.github.com>
2024-01-02 13:06:07 +0400

    Update ini2.py

--- ini2.py
+++ ini2.py
@@ -1,12 +1,18 @@
+# INI2: Variables and Some Arithmetic
+# Given: Two positive integers a and b, each less than 1000.
+# Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths 
+# a and b.
+
 import math
 
 def hypoteneuse (a, b):
-    # a = int(input("a"))
-    # b = int(input("b"))
     if a <1000 and b <1000:  
-        c_sq = pow(a, 2) + pow(b, 2)
-        # c = math.sqrt(c_sq)     
+        c_sq = pow(a, 2) + pow(b, 2)  
     return c_sq
 
-print(hypoteneuse(904, 837))
-# ans: 34
+ros_f = open("/Users/ . . /Downloads/rosalind_ini4.txt")
+a, b = map(int, ros_f.readline().rstrip().rsplit())
+print(hypoteneuse(a, b))
+
+# print(hypoteneuse(904, 837))
+# Ans: 34
