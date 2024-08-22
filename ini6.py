e88198f4f5974ebd60856c70026be0ab6beb4f94
gabrielle ysabel <98463078+bakuncwa@users.noreply.github.com>
2024-08-22 11:44:06 +0800

    Update ini6.py removed try-catch

--- ini6.py
+++ ini6.py
@@ -12,17 +12,12 @@ def wordCount(s):
             dictCount[x] = dictCount.get(x,0) + 0
         dictCount[x] = dictCount.get(x,0) + 1
     return dictCount
-    
 
-try:
-    ros_f = open("/Users/ . . . /Downloads/rosalind_ini6.txt")
-    try:
-        s = ros_f.readline().rstrip()
-        if ros_f.count > 3000:
-            print("s must not be longer than 3000 characters.")
-    except:
-        dictCount = wordCount(s)
-        for k, v in dictCount.items():
-            print(k,v)
-except:
-    print("File did not open correctly.")
+ros_f = open("/Users/ . . . /Downloads/rosalind_ini6.txt")
+s = ros_f.readline().rstrip()
+if ros_f.count > 3000:
+    print("s must not be longer than 3000 characters.")
+
+dictCount = wordCount(s)
+    for k, v in dictCount.items():
+        print(k,v)
