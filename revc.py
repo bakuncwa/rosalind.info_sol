a92468cb99a5492015b69f3e9a81cead8e970283
gabrielle ysabel <98463078+bakuncwa@users.noreply.github.com>
2024-08-23 03:20:26 +0800

    Update revc.py variable name change

--- revc.py
+++ revc.py
@@ -2,18 +2,18 @@
 # Given:  A DNA string s of length at most 1000 bp.
 # Return: The reverse complement sc of s.
 
-def reverse_comp(s):
+def reverse_comp(seq):
     comp = {"A": "T", "T": "A", "C": "G", "G": "C"}
     DNA_strand = " "
-    for nuc_base in s[::-1]:
+    for nuc_base in seq[::-1]:
         comp[nuc_base] = comp.get(nuc_base, 0)
         DNA_strand += comp[nuc_base]
     return DNA_strand
 
 ros_f = open("/Users/. . . /Downloads/rosalind_revc.txt")
-s = ros_f.readline().rstrip()
+seq = ros_f.readline().rstrip()
 if ros_f.count > 1000:
     print("s must not be longer than 1000 bp.")
 else:
-    DNA_strand = reverse_comp(s)
+    DNA_strand = reverse_comp(seq)
     print(DNA_strand)
